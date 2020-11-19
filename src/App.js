610cba8bfcd581aa2b2ff4ca75af177715a4f7a5
OctoOcto <66621960+OctoOcto@users.noreply.github.com>
2020-11-19 14:06:36 +0100

    pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // css
 import './assets/css/App.css';
@@ -12,53 +12,82 @@ import Footer from './Components/Footer';
 import Form from './Components/Form';
 import SchoolCard from './Components/SchoolCard';
 
+const fetchData = async (state, query, page = 1) => {
+	if (!state || !query) {
+		throw new Error('No query');
+	}
+
+	const response = await axios.get(
+		`https://api.schooldigger.com/v1/schools?st=${state}&q=${query}&page=${page}&appID=${process.env.REACT_APP_APP_ID}&appKey=${process.env.REACT_APP_API_KEY}`
+	);
+
+	return response.data;
+};
+
 function App() {
+	const [result, setResult] = useState({});
 	const [stateInput, setStateInput] = useState('');
 	const [schoolInput, setSchoolInput] = useState('');
+	const [page, setPage] = useState(1);
 
-	const [result, setResult] = useState([]);
-
-	// Fetchdata
-	const fetchData = async () => {
-		try {
-			const response = await axios.get(
-				`https://api.schooldigger.com/v1/schools?st=${stateInput}&q=${schoolInput}&appID=${process.env.REACT_APP_APP_ID}&appKey=${process.env.REACT_APP_API_KEY}`
-			);
-
-			setResult(response.data);
-		} catch (e) {
-			alert('An error occurred');
+	useEffect(() => {
+		if (stateInput.length > 0 && schoolInput.length > 0) {
+			fetchData(stateInput, schoolInput, page).then((data) => {
+				setResult(data);
+			});
 		}
-	};
+	}, [page]);
 
 	return (
 		<div className='App'>
-			<div className='wrapper'>
-				<header>
-					<Header />
-				</header>
-				<div className='form-wrapper'>
-					<Form
-						setStateInput={setStateInput}
-						setSchoolInput={setSchoolInput}
-						handleSubmit={(e) => {
-							e.preventDefault();
-							fetchData();
-						}}
-					/>
-				</div>
-				<div className='result-container'>
-					<p>{result.numberOfSchools} school(s) found</p>
-				</div>
-				<main>
-					{result.length === 0 ? (
-						<div></div>
-					) : (
-						result.schoolList.map((infos, i) => {
-							return <SchoolCard infos={infos} key={i} />;
-						})
-					)}
-				</main>
+			<header>
+				<Header />
+			</header>
+			<div className='form-wrapper'>
+				<Form
+					setStateInput={setStateInput}
+					setSchoolInput={setSchoolInput}
+					handleSubmit={async (e) => {
+						e.preventDefault();
+						try {
+							const result = await fetchData(stateInput, schoolInput, page);
+							setResult(result);
+						} catch (e) {
+							console.error(e);
+						}
+					}}
+				/>
+			</div>
+			<div className='result-container'>
+				<p>
+					{result.numberOfSchools ? result.numberOfSchools : 0} school(s) found
+				</p>
+			</div>
+			<main>
+				{result.schoolList ? (
+					result.schoolList.map((infos, i) => {
+						return <SchoolCard infos={infos} key={i} />;
+					})
+				) : (
+					<div />
+				)}
+			</main>
+
+			<div>
+				<button
+					type='button'
+					onClick={() => {
+						setPage((page) => page - 1);
+					}}>
+					Previous
+				</button>
+				<button
+					type='button'
+					onClick={() => {
+						setPage((page) => page + 1);
+					}}>
+					Next
+				</button>
 			</div>
 			<footer>
 				<Footer />
